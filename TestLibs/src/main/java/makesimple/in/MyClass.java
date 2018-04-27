e444c87aed1dccc8df82952a60564dfc26261c3e
Nikhil <nikhilpsathyanathan@gmail.com>
2018-04-27 10:19:23 +0530

    Initial commit

---
+++ TestLibs/src/main/java/makesimple/in/MyClass.java
@@ -0,0 +1,14 @@
+package makesimple.in;
+
+/**
+ * Created by asus on 27-04-2018.
+ */
+
+public class MyClass {
+
+    public static String Test(String a)
+    {
+        return  a+" hi";
+    }
+
+}
